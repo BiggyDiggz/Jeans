2c0335760f9932dc851fcf5327198053aefb0047
BiggyDiggz <do.william+github@gmail.com>
2016-03-12 21:38:57 -0800

    Added license to top of code.

--- jeans/Jeans.js
+++ jeans/Jeans.js
@@ -1,10 +1,26 @@
-/*jslint         browser : true, continue : true,
- devel  : true, indent  : 2,    maxerr   : 50,
- newcap : true, nomen   : true, plusplus : true,
- regexp : true, sloppy  : true, vars     : false,
- white  : true
+/**
+ * The MIT License (MIT)
+
+ Copyright (c) 2016 William Do
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
  */
-/*global $, Jeans */
 
 var Jeans = (function() {
 
@@ -18,7 +34,7 @@ var Jeans = (function() {
 	var hardwareAccelerate = "hardwareAccelerate";
 	var animationObjects = [];
 
-	/* Matthew Lein's easing functions
+	/** Matthew Lein's easing functions
 	 * http://matthewlein.com/ceaser/
 	 * @matthewlein
 	 */
@@ -153,9 +169,10 @@ var Jeans = (function() {
 
 	function createTransition(obj) {
 		var time = obj.time || 0, delay = obj.delay || 0;
+		var ease = obj.ease || "linear";
 		obj.element.style.transitionProperty = getTransitionProperties(obj.tweenObj);
 		obj.element.style.transitionDuration = time + "s";
-		obj.element.style.transitionTimingFunction = obj.ease || "linear";
+		obj.element.style.transitionTimingFunction = eases[ease] || ease;
 		obj.element.style.transitionDelay = delay + "s";
 	}
 
@@ -191,7 +208,7 @@ var Jeans = (function() {
 			properties.push("-webkit-filter");
 			properties.push("filter");
 		}
-		
+
 		return properties.join(", ");
 	}
 
